f492d55d70ec52feb86126f8f23f186bebc488a5
tinvo1101 <tinnvo1101@gmail.com>
2015-08-29 13:16:17 -0500

    Download and Display Web Content

---
+++ Dowloading Web Content/Dowloading Web Content/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Dowloading Web Content
+//
+//  Created by Tin N Vo on 8/29/15.
+//  Copyright © 2015 Tin Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")!
+        
+        webView.loadRequest(NSURLRequest(URL: url))
+        
+        /*
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            // This runs when task completes
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.webView.loadHTMLString(String(webContent!), baseURL: nil)
+                })
+                
+                
+            } else {
+                // Error
+            }
+        }
+        
+        task.resume()
+        */
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
