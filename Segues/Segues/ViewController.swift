f28f805a5b0803aabe3430381e9b232edb40c6f9
tinvo1101 <tinnvo1101@gmail.com>
2015-08-29 11:54:23 -0500

    Basic Multi Views Control & Segues

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Tin N Vo on 8/29/15.
+//  Copyright © 2015 Tin Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
